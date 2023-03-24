6fb71af1db340ad901cd7aadb54dd94b15eea695
ftrbnd <giosalas25@gmail.com>
2023-03-24 13:46:17 -0700

    Finish hovering drop-down menu

--- drown-down-menus/script.js
+++ drown-down-menus/script.js
@@ -1,6 +1,20 @@
+const menu = document.querySelector('.menu');
 const triggerTab = document.querySelector('.trigger');
-triggerTab.addEventListener('click', () => {
-    const menu = document.querySelector('.menu');
+
+triggerTab.addEventListener('mouseover', () => {
+    menu.classList.toggle('visible');
+    menu.classList.toggle('invisible');
+});
+
+triggerTab.addEventListener('mouseout', () => {
+    if (menu.classList.contains('visible') && menu.matches(':hover')) return;
+
+    menu.classList.toggle('visible');
+    menu.classList.toggle('invisible');
+});
+
+menu.addEventListener('mouseout', () => {
+    if (menu.classList.contains('visible') && menu.matches(':hover')) return;
 
     menu.classList.toggle('visible');
     menu.classList.toggle('invisible');
