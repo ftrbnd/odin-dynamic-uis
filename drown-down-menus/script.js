b499c1d9a9547328f734fafde54b8367982e808a
ftrbnd <giosalas25@gmail.com>
2023-03-24 14:43:46 -0700

    Generate html in javascript

--- drown-down-menus/script.js
+++ drown-down-menus/script.js
@@ -1,21 +1,63 @@
-const menu = document.querySelector('.menu');
-const triggerTab = document.querySelector('.trigger');
+loadApp();
 
-triggerTab.addEventListener('mouseover', () => {
-    menu.classList.toggle('visible');
-    menu.classList.toggle('invisible');
-});
+function loadApp() {
+    const TAB_AMOUNT = 4, ITEM_AMOUNT = 4;
+    const dropDownMenu = createDropDownMenu(TAB_AMOUNT, ITEM_AMOUNT);
 
-triggerTab.addEventListener('mouseout', () => {
-    if (menu.classList.contains('visible') && menu.matches(':hover')) return;
+    document.querySelector('body').prepend(dropDownMenu);
+}
 
-    menu.classList.toggle('visible');
-    menu.classList.toggle('invisible');
-});
+function createDropDownMenu(tabAmount = 4, itemAmount = 4) {
+    const dropDownMenu = document.createElement('div');
+    dropDownMenu.classList.add('drop-down-menu');
 
-menu.addEventListener('mouseout', () => {
-    if (menu.classList.contains('visible') && menu.matches(':hover')) return;
+    const navBar = document.createElement('div');
+    navBar.classList.add('nav');
+    for (let i = 1; i <= tabAmount; i++) {
+        const tab = document.createElement('div');
+        tab.classList.add('tab');
+        tab.textContent = 'TAB';
 
-    menu.classList.toggle('visible');
-    menu.classList.toggle('invisible');
-});
+        if (i == tabAmount) {
+            tab.classList.add('trigger');
+
+            tab.addEventListener('mouseover', () => {
+                menu.classList.toggle('visible');
+                menu.classList.toggle('invisible');
+            });
+
+            tab.addEventListener('mouseout', () => {
+                if (menu.classList.contains('visible') && menu.matches(':hover')) return;
+
+                menu.classList.toggle('visible');
+                menu.classList.toggle('invisible');
+            });
+        }
+    
+        navBar.appendChild(tab);
+    }
+
+    const menuContainer = document.createElement('div');
+    menuContainer.classList.add('menu-container');
+    const menu = document.createElement('div');
+    menu.classList.add('menu', 'invisible');
+    for (let i = 1; i <= itemAmount; i++) {
+        const item = document.createElement('div');
+        item.classList.add('item');
+        item.textContent = 'ITEM';
+
+        menu.appendChild(item);
+    }
+    menu.addEventListener('mouseout', () => {
+        if (menu.classList.contains('visible') && menu.matches(':hover')) return;
+
+        menu.classList.toggle('visible');
+        menu.classList.toggle('invisible');
+    });
+    menuContainer.appendChild(menu);
+
+    dropDownMenu.appendChild(navBar);
+    dropDownMenu.appendChild(menuContainer);
+
+    return dropDownMenu;
+}
